417ee777df784fde04bcfd2b10a899384551e63d
vamsikrishnakommineni <vamsi.kommineni95@gmail.com>
2017-02-19 06:33:51 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one.html', function (req,res){
-res.sendFile(path.join(__dirname, 'ui', 'articleone.html'))
+res.sendFile(path.join(__dirname, 'ui', 'article-one.html'))
 });
 
 app.get('/ui/style.css', function (req, res) {
