da7707db294c57cf86176c1c000822eb83d8cc8d
vamsikrishnakommineni <vamsi.kommineni95@gmail.com>
2017-02-21 11:15:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,15 @@ console.log('Loaded!');
 
 var element = document.getElementById('gain');
 element.innerHTML='newvalue';
+
+var img = document.getElementById('madi');
+img.onClick = function(){
+  var interval = setInterval(moveRight, 100);
+ 
+};
+
+function moveRight(){
+    var marginLeft=0;
+    marginLeft=marginLeft+10;
+     img.style.marginLeft = marginLeft+ 'px'; 
+}
