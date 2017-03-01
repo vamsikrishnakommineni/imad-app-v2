2dc73e3794a28b545997a88ce47524c75f036be0
vamsikrishnakommineni <vamsi.kommineni95@gmail.com>
2017-03-02 04:06:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,65 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var articles = {
-    'article-ones' : {
-title:'artickjsdncks',
-heading:'article1',
-date:'20/02/17',
-body:     '    hello, vamsi krishna is a bad boy. Please leave'
-},
-'article-two' : {
- title:'articcdsc',
- heading:'article2',
- date:'21/02/17',
- body:     '    hello, vamsi krishna is a good boy. Please leave'
-},
-'article-three' : {
- title:'articcsa',
- heading:'article3',
- date:'22/02/17',
- body:     '    hello, vamsi krishna is a bad and good boy. Please leave'
-}
-};
 
-function createTemplate(data){
-    var title=data.title;
-    var heading=data.heading;
-    var date=data.date;
-    var body=data.body;
-var htmlTemplate= `
-<html>
-    <head>
-        <title>
-            ${title}
-        </title>
-     <link href="/ui/style.css" rel="stylesheet"/>
-    </head>    
-        <body>
-            <div class="container">
-            <div>
-                <h1>${heading}</h1>
-            </div>
-            <div>
-                <h2>${date}</h2>
-            </div>
-            <hr/>
-            <div>
-            <p>
-                ${body}
-            </p>
-            </div>
-            </div>
-        </body>
-    
- </html>
- `;
- return htmlTemplate;
-}
-app.get('/:articleName', function (req,res){
-    var articleName=req.params.articleName;
-res.send(createTemplate(articles[articleName]));
-});
 
 var counter=0;
 app.get('/counterplus',function(req, res){
