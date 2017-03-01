62b6a167fce5eb0a61fc417198c7b69fafc0e0da
vamsikrishnakommineni <vamsi.kommineni95@gmail.com>
2017-03-02 03:54:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,6 +68,11 @@ app.get('/:articleName', function (req,res){
     var articleName=req.params.articleName;
 res.send(createTemplate(articles[articleName]));
 });
+var counter=0;
+app.get('/counter',function(req,res){
+   counter=counter+1;
+   res.send(counter.toString());
+});
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
