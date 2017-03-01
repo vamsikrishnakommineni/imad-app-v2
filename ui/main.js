e1d9a46667629eac7e0c96199a34ea260eca1039
vamsikrishnakommineni <vamsi.kommineni95@gmail.com>
2017-03-02 04:44:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,10 +16,20 @@ img.onclick = function(){
  
 };
 
-var counter=0;
+
 var button=document.getElementById('counter');
 button.onclick = function(){
-    counter=counter+1;
-    var span = document.getElementById('count');
+    
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var counter=request.responsetext;
+                var span = document.getElementById('count');
     span.innerHTML = counter.toString();
+            }
+        }
+    };
+    request.open('GET','http://vamsikrishnakommineni.imad.hasura-app.io/counterplus',true);
+    req.send =null;
 };
