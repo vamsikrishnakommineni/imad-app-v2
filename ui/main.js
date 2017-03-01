1201b84f19edcda8d6f90652842e07dc29aabaa7
vamsikrishnakommineni <vamsi.kommineni95@gmail.com>
2017-03-02 04:26:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,7 +15,9 @@ img.onclick = function(){
   var interval = setInterval(moveRight,10 );
  
 };
-var counter= document.getElementById('counter');
+
+var counter=0;
+var button=document.getElementById('counter');
 button.onclick = function(){
     counter=counter+1;
     var span = document.getElementById('count');
