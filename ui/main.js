411b251d6cfb3c58efd016d8975c1e38ee551529
vamsikrishnakommineni <vamsi.kommineni95@gmail.com>
2017-03-09 15:01:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,33 +34,29 @@ button.onclick = function(){
     request.send(null);
 };
 
-
-var button=document.getElementById('counter');
-button.onclick = function(){
+var nameinput = document.getElementById('name');
+var names=nameinput.value;
+var submit = document.getElementById('submit');
+submit.onclick = function(){
     
     var request = new XMLHttpRequest();
     request.onreadystatechange = function(){
         if(request.readyState === XMLHttpRequest.DONE){
             if(request.status === 200){
-                var nameinput = document.getElementById('name');
-var names=nameinput.value;
-var submit = document.getElementById('submit');
-submit.onclick = function(){
-    names=['name1','name2','name3','name4'];
+                
+   var names=request.responseText;
+    names=JSON.parse(names);
     list='';
     for(i=0;i<names.length;i++){
         list += '<li>'+names[i]+'</li>';
     }
     var ul=document.getElementById('namelist');
     ul.innerHTML = list;
-};
-                var counter=request.responseText;
-                var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
-            }
-        }
-    };
-    request.open('GET','http://vamsikrishnakommineni.imad.hasura-app.io/counterplus',true);
+
+            }}
+            };
+          
+    request.open('GET','http://vamsikrishnakommineni.imad.hasura-app.io/submit-name?name=' +name ,true);
     request.send(null);
 };
 
